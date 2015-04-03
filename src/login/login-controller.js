eb061881ccc288f80158101c933c6b803c0b5397
Fyodor Khruschov <name.xru@gmail.com>
2015-04-03 17:40:41 +0300

    creating correct login object

--- src/login/login-controller.js
+++ src/login/login-controller.js
@@ -3,6 +3,17 @@
  */
 angular.module('isf.login')
 
-.controller('login-controller', function(){
+.controller('login-controller', function($scope, server){
 
-});
+    $scope.user = {
+      grant_type: 'password',
+      cliend_id: 'testclient2'
+    };
+
+    $scope.login = function(){
+      console.log($scope.user);
+      //server.post('/oauth', $scope.user);
+    }
+
+
+  });
