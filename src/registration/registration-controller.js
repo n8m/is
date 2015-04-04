3475ec89f3ac84e65abfcfd6f45c8078cac9ae33
Fyodor Khruschov <name.xru@gmail.com>
2015-04-04 17:42:08 +0300

    test

--- src/registration/registration-controller.js
+++ src/registration/registration-controller.js
@@ -14,6 +14,9 @@ angular.module('isf.registration')
     server.post('/api/profile/registration', $scope.user).then(function(){
       $state.go('base.regSuccess', {uniqueUrl: $scope.user.dataCredentials.uniqueUrl})
     }, function(response){
+
+      console.log(response);
+
       if(response.status === 400){
         $scope.registrationErrors = response.errors;
       }
