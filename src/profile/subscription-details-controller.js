b170098a56b2fe4d23ee7e7b8efc2ee7d6ec455c
Fyodor Khruschov <name.xru@gmail.com>
2015-04-04 19:51:44 +0300

    pages layout

--- src/profile/subscription-details-controller.js
+++ src/profile/subscription-details-controller.js
@@ -3,6 +3,25 @@
  */
 angular.module('isf.profile')
 
-  .controller('subscription-details-controller', function($scope){
+  .controller('subscription-details-controller', function($scope, auth, server, $rootScope){
+
+    var token = auth.getToken();
+
+    if(token){
+      server.get('/api/profile/login/' + token).then(function(data){
+
+        server.get('/api/profile/cabinet/' + data.id).then(function(data){
+          console.log(data);
+        });
+
+        $rootScope.loggedIn = true;
+      })
+    } else{
+      $rootScope.loggedIn = false;
+      //$state.go('base.main');
+    }
+
+
+
 
   });
