96e5dcdc012b18b347029e679b1c4b9b70dd2e53
Fyodor Khruschov <name.xru@gmail.com>
2015-04-04 14:53:10 +0300

    creating main page

--- src/router/router.js
+++ src/router/router.js
@@ -6,30 +6,34 @@ angular.module('isf.router', [])
 .config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('base.dashboard', {
-      url: 'dashboard',
+      url: '/dashboard',
       templateUrl: 'dashboard/dashboard.html',
       controller: 'dashboard-controller'
     })
     .state('base', {
-      url: '/',
+      abstract: true,
       templateUrl: 'base/base.html'
     })
+    .state('base.main', {
+      url: '/',
+      templateUrl: 'main/main.html'
+    })
     .state('base.pricing', {
-      url: 'pricing',
+      url: '/pricing',
       templateUrl: 'registration/pricing.html'
     })
     .state('base.registration', {
-      url: 'profile/registration',
+      url: '/profile/registration',
       templateUrl: 'registration/registration.html',
       controller: 'registration-controller'
     })
     .state('base.regSuccess', {
-      url: 'profile/registration/success?uniqueUrl',
+      url: '/profile/registration/success?uniqueUrl',
       templateUrl: 'registration/registration-success.html',
       controller: 'registration-success-controller'
     })
     .state('base.login',{
-      url: 'profile/login',
+      url: '/profile/login',
       controller: 'login-controller',
       templateUrl: 'login/login.html'
     });
