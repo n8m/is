a3807f8801c556e33a6caf624e44046cdefc069d
Fyodor Khruschov <name.xru@gmail.com>
2015-04-04 19:39:59 +0300

    receiving settings for cabinet

---
+++ src/profile/profile-settings-controller.js
@@ -0,0 +1,19 @@
+/**
+ * Created by fyodorkhruschov on 02.04.15.
+ */
+angular.module('isf.profile')
+
+.controller('profile-settings-controller', function($scope, auth, server){
+
+    var token = auth.getToken();
+
+    if(token){
+      server.get('/api/profile/login/' + token).then(function(data){
+        $rootScope.loggedIn = true;
+      })
+    } else{
+      $rootScope.loggedIn = false;
+      //$state.go('base.main');
+    }
+
+});
--- src/profile/profile-settings.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by fyodorkhruschov on 02.04.15.
- */
-angular.module('isf.profile')
-
-.controller('profile-settings-controller', function($scope){
-
-});
