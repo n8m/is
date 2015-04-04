b8b34ea4b55d2e77634d46e4398e62560cca886c
Fyodor Khruschov <name.xru@gmail.com>
2015-04-04 16:13:51 +0300

    basic activation scenario

--- src/activation/activation-controller.js
+++ src/activation/activation-controller.js
@@ -3,6 +3,21 @@
  */
 angular.module('isf.activation')
 
-.controller('activation-controller', function($state, $stateParams){
-  console.log($stateParams.token);
+.controller('activation-controller', function($state, $stateParams, server){
+
+    var token = $stateParams.token;
+
+    if(token){
+      server.get('/api/profile/activate/' + token).then(function(data){
+        console.log(data);
+      }, function(response){
+        console.log(response);
+      })
+
+    } else{
+      $state.go('base.main');
+    }
+
+
+
 });
