50341369de28fb5d3aeed76410737d65cc51e104
Fyodor Khruschov <name.xru@gmail.com>
2015-04-10 12:35:26 +0300

    fixnishing pasword reset flow

--- account/src/reset-password/request-reset-password-controller.js
+++ account/src/reset-password/request-reset-password-controller.js
@@ -6,9 +6,12 @@ angular.module('isf.reset-password')
 .controller('request-reset-password-controller', function($scope, server){
   $scope.requestPasswordReset = function(){
 
+    delete $scope.successMessage;
+    delete $scope.errorMessage;
+
     console.log('here');
 
-    server.post('/api/profile/reset/password', {"action" : "create", "username" : $scope.username}).then(function(data){
+    server.post('/api/profile/reset/password', {"action" : "create", "username" : 'my' + $scope.username}).then(function(data){
       $scope.successMessage = true;
     }, function(response){
       $scope.errorMessage = true;
--- account/src/reset-password/reset-password-controller.js
+++ account/src/reset-password/reset-password-controller.js
@@ -3,6 +3,25 @@
  */
 angular.module('isf.reset-password')
 
-  .controller('reset-password-controller', function($stateParams){
-    console.log($stateParams.token);
-  });
+.controller('reset-password-controller', function($scope, $stateParams, server){
+    $scope.setNewPassword = function(){
+
+      delete $scope.successMessage;
+      delete $scope.errorMessage;
+
+      var payload =  {
+        "action" : "update",
+        "key" : $stateParams.token,
+        "password" : $scope.password,
+        "passwordRepeat": $scope.passwordRepeat
+      };
+
+      server.post('/api/profile/reset/password/' + $stateParams.token, payload).then(function(data){
+        $scope.successMessage = true;
+        console.log(data);
+      }, function(response){
+        $scope.errorMessage = true;
+        console.log(response)
+      });
+    }
+});
