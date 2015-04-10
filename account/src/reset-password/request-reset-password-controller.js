7182f389704f78f1db9fa990037fd30d9b707065
Fyodor Khruschov <name.xru@gmail.com>
2015-04-10 12:20:29 +0300

    request password reset

--- account/src/reset-password/request-reset-password-controller.js
+++ account/src/reset-password/request-reset-password-controller.js
@@ -3,6 +3,15 @@
  */
 angular.module('isf.reset-password')
 
-.controller('request-reset-password-controller', function(){
+.controller('request-reset-password-controller', function($scope, server){
+  $scope.requestPasswordReset = function(){
 
+    console.log('here');
+
+    server.post('/api/profile/reset/password', {"action" : "create", "username" : $scope.username}).then(function(data){
+      $scope.successMessage = true;
+    }, function(response){
+      $scope.errorMessage = true;
+    })
+  }
 });
