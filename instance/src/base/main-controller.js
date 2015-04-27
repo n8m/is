2dec6ff42534521b132ce3bdaaaaaead4e1405fe
Fyodor Khruschov <name.xru@gmail.com>
2015-04-27 23:53:42 +0300

    fix

--- instance/src/base/main-controller.js
+++ instance/src/base/main-controller.js
@@ -3,11 +3,14 @@
  */
 angular.module('isfi.main')
 
-.controller('main-controller', function($scope, $state, userProfile, auth, assets){
+.controller('main-controller', function($scope, $state, userProfile, auth, assets, server){
 
     $scope.userProfile = userProfile.getUserProfile();
     $scope.logout = auth.logout;
-    $scope.categories = assets.getCategories();
+
+    server.get('/api/asset/category', {instanceUrl: userProfile.getInstanceUrl()}).then(function(data){
+      $scope.categories = data._embedded.asset_category;
+    });
 
 
     //refactor!
